0612b75f32470a23806b613d970ae6541dd09d61
farrahpepino <idcfarrah@gmail.com>
2025-12-01 13:24:53 -0500

    fileio

--- FileIO/Program.cs
+++ FileIO/Program.cs
@@ -2,10 +2,7 @@
 
 class Program{
     static void Main(string[] args){
-         string path = Path.Combine(
-            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
-            "TestFiles"
-        );
+        string path = @"C:\Users\farrahpepino\Documents\TestFiles";
 
         Directory.CreateDirectory(path);
 
@@ -15,7 +12,12 @@ class Program{
         File.WriteAllText(filePath, "Farrah was here.\n");
         File.AppendAllText(filePath,  "No she is not.\n");
         File.AppendAllText(filePath,  "Yes, she is.\n");
+        
+        if (File.Exists(filePath)){
+            string content = File.ReadAllText(filePath);
+            Console.WriteLine(content);
 
+        }
         Console.ReadLine();
     }
 }
