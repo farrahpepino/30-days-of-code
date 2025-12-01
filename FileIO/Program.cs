51a15c1281cb3b073b5c6933134fa7e88aa1d9c3
farrahpepino <idcfarrah@gmail.com>
2025-12-01 13:14:11 -0500

    simple FileIo

---
+++ FileIO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program{
+    static void Main(string[] args){
+         string path = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+            "TestFiles"
+        );
+
+        Directory.CreateDirectory(path);
+
+        string fileName = "log.txt";
+        string filePath = Path.Combine(path, fileName);
+
+        File.WriteAllText(filePath, "Farrah was here.\n");
+        File.AppendAllText(filePath,  "No she is not.\n");
+        File.AppendAllText(filePath,  "Yes, she is.\n");
+
+        Console.ReadLine();
+    }
+}
+
