1e468b40d500e82edc856f682dddd9c9644e7356
farrahpepino <idcfarrah@gmail.com>
2025-12-05 13:00:34 -0500

    studying ascii

---
+++ Frequency/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] freq = new int[26];
+Console.WriteLine(string.Join(", ", freq));
+
+string s = "farrah";
+string t = "harraf";
+
+
+foreach (char c in s){
+    Console.WriteLine(c);
+    Console.WriteLine(freq[c-'a']++);
+}
+Console.WriteLine(string.Join(", ", freq));
+
+
+foreach (char c in t){
+    Console.WriteLine(c);
+    Console.WriteLine(freq[c-'a']--);
+}
+
+Console.WriteLine(string.Join(", ", freq));
+
+foreach (int i in freq){
+    Console.WriteLine(i);
+}
