5c31d8a53ef24ad193a4ed985f197638212d8338
farrahpepino <idcfarrah@gmail.com>
2025-12-12 12:42:36 -0500

    learned how to print binary values

---
+++ BinaryValues/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> binaryvalues = new List<string>();
+
+        for (int i = 0; i <= 12; i++) 
+        {
+            string stringRep = Convert.ToString(i, 2);
+            Console.WriteLine(stringRep);
+            binaryvalues.Add(stringRep);
+        }
+
+        foreach (var val in binaryvalues)
+        {
+            int e = Convert.ToInt32(val, 2);
+            Console.WriteLine(e);
+        }
+    }
+}
